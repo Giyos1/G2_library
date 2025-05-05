8ce5c717f04c09ce24af78ab46f5c7676f963fbe
Giyos1 <giyosoripov4@gmail.com>
2025-05-05 19:24:41 +0500

    add middlware

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accounts.middleware.LogIPMiddleware',
+    # 'accounts.middleware.BlockingMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
