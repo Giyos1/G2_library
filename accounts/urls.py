d385242923864269e1470e9e0be3120fb12aba82
G'iyos Master <86424045+Giyos1@users.noreply.github.com>
2025-06-20 20:13:16 +0500

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path("login/", views.login_user, name='login'),
     path("logout/", views.logout_user, name='logout'),
-    path("file_upload/", views.file_upload, name='file_uploads'),
-    path("file_list/", views.file_list, name='file_list'),
-    path('forgot_password/', views.ForgotPassword.as_view(), name='forgot_password'),
-    path('restore_password', views.RestorePassword.as_view(), name='restore_password')
+    # path("file_upload/", views.file_upload, name='file_uploads'),
+    # path("file_list/", views.file_list, name='file_list'),
+    # path('forgot_password/', views.ForgotPassword.as_view(), name='forgot_password'),
+    # path('restore_password', views.RestorePassword.as_view(), name='restore_password')
 ]
